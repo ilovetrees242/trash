7fc60b110d7291b4f8d36853c9797366c8450122
ilovetrees242 <riyrty594@gmail.com>
2026-06-10 22:42:39 +0530

    Made it work

--- main.c
+++ main.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/wait.h>
 
+char cmd[256] = "";
+char *args[128] = {};
+
+void splitArgs(const char *data){
+    int counter = 0;
+    int argCounter = 0;
+    char buffer[128] = "";
+        printf("%s\n", args[0]);
+    for(int i = 0; i < strlen(data); i++){
+        if ( data[i] == ' ' ){
+            args[argCounter] = buffer;
+            argCounter++;
+            strcpy(buffer, "");
+            counter = 0;
+            continue;
+        }
+        buffer[counter] = data[i];
+        counter++;
+    }
+}
+bool runBuiltIn(const char *cmd){
+    int builtIn = strcmp(cmd, "exit");
+    if ( builtIn == 0 ){
+        exit(0);
+    }
+}
 int main(){
-    printf("So trash");
+    char input[50] = "";
+    while ( true ) {
+        printf("> ");
+        fgets(input, sizeof(input), stdin);
+        input[strlen(input) - 1] = '\0';
+        runBuiltIn(input);
+        pid_t pid = fork();
+        if ( pid == 0 ){
+            execvp(input, NULL);
+        }
+        else {
+            wait(NULL);
+        }
+    }
 }
